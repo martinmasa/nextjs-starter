1801ceff11069037d5cc7422099204b462f61def
Martin <martin.masa10@hotmail.com>
2018-11-27 23:57:20 +0000

    setup: scaffold minimal next js application

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>This is the home page!</p>
+      </div>
+    )
+  }
+}
+
+export default Home
